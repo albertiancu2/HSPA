37841d0726e7c454580dbd20b2173d2905fb3a05
Albert <albert.nico142@gmail.com>
2023-04-02 12:26:59 +0300

    added animations

--- Frontend/src/app/property/property card/property-card.component.ts
+++ Frontend/src/app/property/property card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class PropertyCardComponent {
-  Property: any = {
-    "Id": 1,
-    "Type": "House",
-    "Name": "BBB",
-    "Price": 120000
-  }
+  @Input() property: any
 }
