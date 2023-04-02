6c26a6a14986579aa89b2337f643f1b47ad36d6c
Albert <albert.nico142@gmail.com>
2023-04-02 12:26:20 +0300

    added animation

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Type": "House",
+      "Name": "BBB",
+      "Price": 120000
+    },
+    {
+      "Id": 2,
+      "Type": "Apartment",
+      "Name": "ccc",
+      "Price": 23112333
+    },
+    {
+      "Id": 3,
+      "Type": "House",
+      "Name": "ddd",
+      "Price": 120000
+    },
+    {
+      "Id": 4,
+      "Type": "House",
+      "Name": "gggg",
+      "Price": 120000
+    }
+
+  ]
 }
