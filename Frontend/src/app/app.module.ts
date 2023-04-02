c530b2ef571d2f9a541b19d67df62927dfceab31
Albert <albert.nico142@gmail.com>
2023-04-02 15:13:30 +0300

    get houses from json file

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HousingService } from './service/housing.service';
+
 
 @NgModule({
   declarations: [
@@ -12,11 +15,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     PropertyCardComponent,
     PropertyListComponent,
     NavBarComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HousingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,37 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/service/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
 
-  properties: Array<any> = [
-    {
-      "Id": 1,
-      "Type": "House",
-      "Name": "BBB",
-      "Price": 120000
-    },
-    {
-      "Id": 2,
-      "Type": "Apartment",
-      "Name": "ccc",
-      "Price": 23112333
-    },
-    {
-      "Id": 3,
-      "Type": "House",
-      "Name": "ddd",
-      "Price": 120000
-    },
-    {
-      "Id": 4,
-      "Type": "House",
-      "Name": "gggg",
-      "Price": 120000
-    }
+  properties: any;
 
-  ]
+  constructor(private housingService: HousingService) {}
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data=> {
+        this.properties = data;
+        console.log(data)
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
 }
